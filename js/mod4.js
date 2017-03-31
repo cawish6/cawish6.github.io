5b5b183ddc6d011a046d8318a64d2b5522440998
cawish6 <cawish6@aol.com>
2017-03-31 13:09:54 -0400

    Update mod4.js

--- js/mod4.js
+++ js/mod4.js
@@ -1,1 +1,11 @@
 //
+var number = 1;
+var outputDiv=document.createElement('div');
+document.body.appendChild(outputDiv);
+while (number <= 5) {
+
+  console.log(number);
+  outputDiv.innerHTML+="<br>"+number+"</br>";
+  number++;
+
+}
